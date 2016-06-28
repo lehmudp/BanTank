cccb0a414c9fb032bf5dfad5ee9f2057f3dc4030
lehmudp <tranhieu3101994@gmail.com>
2016-06-28 21:34:30 +0700

    update tutorial 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,41 @@
-var context;
+var TankOnline = {};
+window.onload = function() {
+  TankOnline.game = new Phaser.Game(window.innerWidth,
+                                    window.innerHeight,
+                                    Phaser.AUTO,
+                                    '',
+                                    {preload: preload, create: create, update: update});
+}
 
-window.onload = function(){
-    var canvas = document.createElement("canvas");
-    document.body.appendChild(canvas);
-    
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+var tank;
+var preload = function() {
+  TankOnline.game.load.image('tankDown', './images/tank_player1_down_c0_t1_s1.png');
+  TankOnline.game.load.image('tankUp', './images/tank_player1_up_c0_t1_s1.png');
+  TankOnline.game.load.image('tankLeft', './images/tank_player1_left_c0_t1_s1.png');
+  TankOnline.game.load.image('tankRight', './images/tank_player1_right_c0_t1_s1.png');
 
-  context = canvas.getContext("2d");
+}
+
+var create = function(){
+  tank = new Tank(window.innerWidth/2, window.innerHeight/2);
+  tank2 = new Tank(window.innerWidth/2, window.innerHeight/2 - 100);
+  TankOnline.game.physics.startSystem(Phaser.Physics.ARCADE);
+  TankOnline.game.physics.arcade.enable(tank);
+
+  TankOnline.keyboard = TankOnline.game.input.keyboard;
+}
+
+var update = function(){
+  var directionX, directionY;
+  if(TankOnline.keyboard.isDown(Phaser.KeyCode.LEFT)) directionX = -1;
+  else if(TankOnline.keyboard.isDown(Phaser.KeyCode.RIGHT)) directionX = 1;
+  else direcionX = 0
+
+  if(TankOnline.keyboard.isDown(Phaser.KeyCode.UP)) directionY = -1;
+  else if(TankOnline.keyboard.isDown(Phaser.KeyCode.DOWN)) directionY = 1;
+  else direcionY = 0
+
+  tank.update(directionX, directionY);
+  tank2.update(directionX, directionY);
+
 }
