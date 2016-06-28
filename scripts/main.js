156bfa9482ad5ac33518d30346d2ea0b1110373c
lehmudp <tranhieu3101994@gmail.com>
2016-06-28 23:14:46 +0700

    update bullet, bai tap ve nha

--- scripts/main.js
+++ scripts/main.js
@@ -8,19 +8,24 @@ window.onload = function() {
 }
 
 var tank;
+var bullet;
 var preload = function() {
   TankOnline.game.load.image('tankDown', './images/tank_player1_down_c0_t1_s1.png');
   TankOnline.game.load.image('tankUp', './images/tank_player1_up_c0_t1_s1.png');
   TankOnline.game.load.image('tankLeft', './images/tank_player1_left_c0_t1_s1.png');
   TankOnline.game.load.image('tankRight', './images/tank_player1_right_c0_t1_s1.png');
+  TankOnline.game.load.image('bullet', './images/bullet_right.png');
 
 }
 
 var create = function(){
+
+  bullet = TankOnline.game.add.sprite(0, window.innerHeight/2,'bullet');
   tank = new Tank(window.innerWidth/2, window.innerHeight/2);
   tank2 = new Tank(window.innerWidth/2, window.innerHeight/2 - 100);
   TankOnline.game.physics.startSystem(Phaser.Physics.ARCADE);
   TankOnline.game.physics.arcade.enable(tank);
+  TankOnline.game.physics.arcade.enable(bullet);
 
   TankOnline.keyboard = TankOnline.game.input.keyboard;
 }
@@ -37,5 +42,10 @@ var update = function(){
 
   tank.update(directionX, directionY);
   tank2.update(directionX, directionY);
+
+  if (TankOnline.keyboard.isDown(Phaser.KeyCode.SPACEBAR)) {
+    bullet.body.velocity.x = 1000;
+  }
+
 
 }
